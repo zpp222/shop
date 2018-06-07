3ef05fbb00a08688ff365b6af674e9ca8429c403
zpp <zhangpengp993@163.com>
2018-06-07 16:56:16 +0800

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-div">
+    {{ msg }} {{ $route.params.name }} <br/>
+    <form @submit.prevent="onsubmit">
+      <input v-model="name" placeholder="edit name"/>
+      <input v-model="passwd" placeholder="edit password" type="password"/>
+      <input type="submit"/>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'login from :'
+    }
+  },
+  methods: {
+    onsubmit: function (event) {
+      console.log('11')
+      alert('submit :' + this.name)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
