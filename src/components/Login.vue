2c9a73d31529b80737c2ee575999bf3d6d748f67
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-06-25 14:11:34 +0800

    密码MD5 优化2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -40,12 +40,11 @@ export default {
       // var randoms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
       // var salt = randoms[Math.floor(Math.random() * 36)]
       let username = this.loginForm.name
-      let pwd = this.loginForm.passwd
-      var salt = Base64.encode(username)
+      let pwd = this.$md5(Base64.encode(this.loginForm.passwd))
+
       this.$http.post('/api/shop-console/login2', {
         name: username,
-        passwd: this.$md5(salt + pwd),
-        salt: salt
+        passwd: pwd
       })
         .then(function (response) { that.submitSuc(response) })
         .catch(function (response) {
